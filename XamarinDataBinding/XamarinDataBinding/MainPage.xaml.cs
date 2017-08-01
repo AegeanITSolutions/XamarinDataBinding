bf61aebe596c698ab64eab08d483ccae76ef0c18
halilkocaerkek <halil.kocaerkek@gmail.com>
2017-08-01 09:11:35 +0300

    First Commit

---
+++ XamarinDataBinding/XamarinDataBinding/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDataBinding
+{
+	public partial class MainPage : ContentPage
+	{
+        DateViewModel context;
+
+        public MainPage()
+		{
+            context = new DateViewModel();
+            context.PropertyChanged += Context_PropertyChanged;
+            this.BindingContext = context;
+
+
+            InitializeComponent();
+		}
+
+        private void Context_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Console.WriteLine(string.Format("{0} : {1} - {2}", e.PropertyName, "", "")); 
+        }
+
+        public void applyChange()
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            applyChange();
+        }
+    }
+}
---
+++ XamarinDataBinding/XamarinDataBinding/ViewModel/DateViewModel.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XamarinDataBinding
+{
+    public class DateViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double minimumDuration;
+        public double MinimumDuration
+        {
+            get { return minimumDuration; }
+            set
+            {
+                if (minimumDuration != value)
+                {
+                    minimumDuration = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("MinimumDuration");
+                    }
+                }
+            }
+        }
+
+        private double maximumDuration;
+        public double MaximumDuration
+        {
+            get { return maximumDuration; }
+            set
+            {
+                if (maximumDuration != value)
+                {
+                    maximumDuration = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("MaximumDuration");
+                    }
+                }
+            }
+        }
+
+        private DateTime approvedApplyDate;
+        public DateTime ApprovedApplyDate
+        {
+            get { return approvedApplyDate; }
+            set
+            {
+                if (approvedApplyDate != value)
+                {
+                    approvedApplyDate = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("ApprovedApproveDate");
+                    }
+                }
+            }
+        }
+
+        private DateTime approvedApproveDate;
+        public DateTime ApprovedApproveDate
+        {
+            get { return approvedApproveDate; }
+            set
+            {
+                if (approvedApproveDate != value)
+                {
+                    approvedApproveDate = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("ApprovedApproveDate");
+                    }
+                }
+            }
+        }
+
+        private DateTime waitingApplyDate;
+        public DateTime WaitingApplyDate
+        {
+            get { return waitingApplyDate; }
+            set
+            {
+                if (waitingApplyDate != value)
+                {
+                    waitingApplyDate = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("WaitingApplyDate");
+                    }
+                }
+            }
+        }
+
+        private DateTime waitingApproveDate;
+        public DateTime WaitingApproveDate
+        {
+            get { return waitingApproveDate; }
+            set
+            {
+                if (waitingApproveDate != value)
+                {
+                    waitingApproveDate = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("WaitingApproveDate");
+                    }
+                }
+            }
+        }
+
+        private int duration;
+        public int Duration
+        {
+            get { return duration; }
+            set
+            {
+                if (duration != value)
+                {
+                    duration = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged("Duration");
+                    }
+                }
+            }
+        }
+
+        public DateViewModel()
+        {
+            minimumDuration = 1;
+            maximumDuration = 100;
+            approvedApplyDate = DateTime.Now.AddDays(-50);
+            approvedApproveDate = DateTime.Now;
+
+            waitingApplyDate = new DateTime(2017, 7, 11);
+            Compute();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                if (propertyName != "WaitingApproveDate" && propertyName != "Duration")
+                {
+                    Compute();
+                }
+
+                if (  propertyName == "Duration")
+                {
+                    WaitingApproveDate = waitingApplyDate.AddDays(Duration);
+ 
+                    PropertyChanged(this, new PropertyChangedEventArgs("WaitingApproveDate"));
+                }
+
+                    PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Compute()
+        {
+            TimeSpan ts = approvedApproveDate - approvedApplyDate;
+            Duration = ts.Days;
+            WaitingApproveDate = waitingApplyDate.AddDays(ts.TotalDays);
+        }
+    }
+}
